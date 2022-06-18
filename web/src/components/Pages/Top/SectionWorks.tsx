95568c862f043de5b61a840b07989cce5ed7932e
Qiushi Pan <17402261+qqhann@users.noreply.github.com>
2022-06-19 00:10:18 +0900

    add: works card style

--- web/src/components/Pages/Top/SectionWorks.tsx
+++ web/src/components/Pages/Top/SectionWorks.tsx
@@ -8,16 +8,29 @@ interface Props {
 }
 export const Works = ({ posts }: Props) => {
   return (
-    <>
-      <h1>Works</h1>
-      {posts.length > 0 &&
-        posts.map((post) => (
-          <Link href={`/works/${post.slug}`} key={post.slug}>
-            <a>
-              <WorksCard post={post} />
-            </a>
-          </Link>
-        ))}
-    </>
+    <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div className="absolute inset-0">
+        <div className="bg-white h-1/3 sm:h-2/3" />
+      </div>
+      <div className="relative max-w-7xl mx-auto">
+        <div className="text-center">
+          <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+            Works
+          </h2>
+          <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+            My works are here.
+          </p>
+        </div>
+        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none"></div>
+        {posts.length > 0 &&
+          posts.map((post) => (
+            <Link href={`/works/${post.slug}`} key={post.slug}>
+              <a>
+                <WorksCard post={post} />
+              </a>
+            </Link>
+          ))}
+      </div>
+    </div>
   )
 }
--- web/src/components/WorksCard.tsx
+++ web/src/components/WorksCard.tsx
@@ -1,8 +1,59 @@
 import { Post } from '~/types'
 
+const __post = {
+  title: 'Boost your conversion rate',
+  href: '#',
+  category: { name: 'Article', href: '#' },
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+  date: 'Mar 16, 2020',
+  datetime: '2020-03-16',
+  imageUrl:
+    'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+  readingTime: '6 min',
+  author: {
+    name: 'Roel Aufderehar',
+    href: '#',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+}
+
 type Props = {
   post: Post
 }
 export const WorksCard = ({ post }: Props) => {
-  return <div>{post.slug}</div>
+  return (
+    <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+      <div className="flex-shrink-0">
+        <img
+          className="h-48 w-full object-cover"
+          src={__post.imageUrl}
+          alt=""
+        />
+      </div>
+      <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+        <div className="flex-1">
+          <p className="text-sm font-medium text-indigo-600">
+            <a href={__post.category.href} className="hover:underline">
+              {__post.category.name}
+            </a>
+          </p>
+          <a href={__post.href} className="block mt-2">
+            <p className="text-xl font-semibold text-gray-900">{post.slug}</p>
+            <p className="mt-3 text-base text-gray-500">{`${post.content}`}</p>
+          </a>
+        </div>
+        <div className="mt-6 flex items-center">
+          <div className="ml-3">
+            <div className="flex space-x-1 text-sm text-gray-500">
+              <time dateTime={__post.datetime}>{__post.date}</time>
+              <span aria-hidden="true">&middot;</span>
+              <span>{__post.readingTime} read</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
