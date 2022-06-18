08e72bffcbfba83dc94c7adb544709295039fdbd
Qiushi Pan <17402261+qqhann@users.noreply.github.com>
2022-06-19 00:52:54 +0900

    chore: extract profile picture component

---
+++ web/src/components/Pages/Top/SectionSelf/SectionSelf.tsx
@@ -0,0 +1,18 @@
+import { ProfilePic } from './ProfilePic'
+
+export const SelfIntroduction = () => {
+  return (
+    <div>
+      <span className="text-lg">Qiushi Pan</span>
+      <p>Name: Qiushi Pan / 潘 秋実</p>
+      <p>
+        My interests are Machine Learning, Consciousness, App Development Hobby
+        I enjoy Anime, Manga, Movies, and Games
+      </p>
+      <p>
+        Links GitHub / Twitter / SpeakerDeck / Zenn / AtCoder Amazon Wish List
+      </p>
+      <ProfilePic />
+    </div>
+  )
+}
