a6667a935a001c9e39a6b6b6f1562a9c265c2a73
Qiushi Pan <qiu.gits@gmail.com>
2022-06-26 14:17:51 +0900

    chore: add google analytics

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,9 +1,32 @@
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 import 'styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script
+        strategy="afterInteractive"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-2SJPH84SZN"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2SJPH84SZN');
+            `,
+        }}
+      />
+    </>
+  )
 }
 
 export default MyApp
