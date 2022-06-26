06bb20fccc19be45ab191041ba081b216b743e97
Qiushi Pan <qiu.gits@gmail.com>
2022-06-26 13:15:37 +0900

    chore: update self introduction.

--- web/src/components/Pages/Top/SectionSelf/SectionSelf.tsx
+++ web/src/components/Pages/Top/SectionSelf/SectionSelf.tsx
@@ -1,18 +1,17 @@
 import { ProfilePic } from './ProfilePic'
 
-const faqs = [
+const intro: { title: string; content: string }[] = [
   {
-    question: 'Name',
-    answer: 'Qiushi Pan / 潘 秋実',
+    title: 'As a researcher',
+    content: `Qiushi Pan (潘 秋実) is a Ph.D. student in Doctoral Programs in Intelligent and Mechanical Interaction Systems at the University of Tsukuba under the supervision of Prof. Taro Tezuka. His research interests include offline reinforcement learning, signal processing, image processing, and educational adaptation.`,
   },
   {
-    question: 'Interest',
-    answer: `My interests are Machine Learning, Consciousness, App Development
-          Hobby I enjoy Anime, Manga, Movies, and Games`,
+    title: 'As a coder',
+    content: `He is an open-source enthusiast. He builds fun projects and publishes them as OSS in his spare time. He made the CommentScreen app and co-founded its company. He writes code in Rust, Python, and TypeScript. He is not only good at frontend but also familiar with backend and cross-platform apps.`,
   },
   {
-    question: 'Links',
-    answer: 'GitHub / Twitter / SpeakerDeck / Zenn / AtCoder Amazon Wish List',
+    title: 'As a gamer',
+    content: `He loves playing games. He used to play the role of regional community manager for the game Vainglory. His interest in games also stimulates his interest in game development and AI.`,
   },
 ]
 
@@ -29,12 +28,14 @@ export const SelfIntroduction = () => {
           </div>
           <div className="mt-12 lg:mt-0 lg:col-span-2">
             <dl className="space-y-12">
-              {faqs.map((faq) => (
-                <div key={faq.question}>
+              {intro.map((faq) => (
+                <div key={faq.title}>
                   <dt className="text-lg leading-6 font-medium text-gray-900">
-                    {faq.question}
+                    {faq.title}
                   </dt>
-                  <dd className="mt-2 text-base text-gray-500">{faq.answer}</dd>
+                  <dd className="mt-2 text-base text-gray-500">
+                    {faq.content}
+                  </dd>
                 </div>
               ))}
             </dl>
