2dccb3afae6b154801260f9347fdfe99f122e058
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2023-08-05 17:17:15 +0900

    chore: chose ssh clone if possible

--- web/public/init.sh
+++ web/public/init.sh
@@ -1,2 +1,9 @@
-git clone https://github.com/qqpann/dotfiles.git ~/.dotfiles
+ssh -T git@github.com > /dev/null 2>&1
+if [ $? -eq 1 ]; then
+  echo "SSH connection to GitHub successful, using SSH for git clone."
+  git clone git@github.com:qqpann/dotfiles.git ~/.dotfiles
+else
+  echo "SSH connection to GitHub failed, using HTTPS for git clone."
+  git clone https://github.com/qqpann/dotfiles.git ~/.dotfiles
+fi
 sh ~/.dotfiles/init.sh
